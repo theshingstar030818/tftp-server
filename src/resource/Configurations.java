00ea901245227e6523f3d6da209f1f6ed5797f0b
Charlie Li <ok>
2016-01-19 00:09:28 -0500

    Added default ports

--- src/resource/Configurations.java
+++ src/resource/Configurations.java
@@ -12,5 +12,6 @@ public class Configurations {
 	
 	public static final String ROOT_FILE_DIRECTORY = System.getProperty("user.home") + "/TFTP-Storage-Folder";
 	public static final int MAX_BUFFER = 512;
-
+	public static final int ERROR_SIM_LISTEN_PORT = 0;  // Change this to something your Unix systems like
+	public static final int SERVER_LISTEN_PORT = 0;		// Ditto line above
 }
