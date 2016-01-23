7e01508b7b2a7c5d763a61365b25f7f8dc33af84
Yike Chen <yikechen@cmail.carleton.ca>
2016-01-23 15:47:26 -0500

    Added constructor

--- src/server/TFTPService.java
+++ src/server/TFTPService.java
@@ -2,6 +2,10 @@
  * 
  */
 package server;
+import java.net.DatagramPacket;
+
+import helpers.FileStorageService;
+import types.RequestType;
 
 /**
  * @author Team 3
@@ -11,11 +15,46 @@ package server;
  */
 public class TFTPService implements Runnable {
 
+	private DatagramPacket inPacket;
+	
 	/**
 	 * 
 	 */
 	public TFTPService() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public TFTPService(DatagramPacket packet) {
+		this.inPacket = packet;
+	}
+	
+	//return type is wrong
+	private void handleFileWriteOperation() {
+
+	}
+	
+	private void handleFileReadOperation() {
+		/*
+		   byte[] data = new byte[4906];
+		   //inPacket = new TFTP(data, data.length);
+		   data = inPacket.getDataBody();
+		   
+		   
+		 */
+	}
+	
+	private void handleOnOperationError() {
+		
+	}
+	
+	private void handleConnectionAcknowledgement() {
+		byte[] a = RequestType.WRQ.getHeaderByteArray();
+	}
+	
+	private byte[] generateResponse(RequestType opCode) {
+		byte[] a = new byte[1];
+		a[0] = 1;
+		return a;
 	}
 	
 	public void run() {
