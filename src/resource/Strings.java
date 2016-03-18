d25140db8158d12d8f264ea3675a2d6aec8a2c05
Yike Chen <yikechen@cmail.carleton.ca>
2016-03-17 20:53:04 -0400

    Fixed merge conflict

--- src/resource/Strings.java
+++ src/resource/Strings.java
@@ -69,6 +69,7 @@ public class Strings {
 	public static final String INVALID_PACKET_SIZE = "Invalid packet size.";
 	public static final String UNKOWN_ERROR_CODE = "Unknown error code.";
 	public static final String INVALID_ERROR_CODE_FORMAT = "Error code does not begin with 0.";
+	public static final String FILE_ALREADY_EXSIT = "File already exsit.";
 
 	// Error simulator messages
 	public static final String ES_START_LISTENING = "Starting to listen for traffic";
